30ebb1505322a9ef18a2590a10aa50a079a10003
Alan <67348179+alantbarboza@users.noreply.github.com>
2026-02-23 23:04:55 -0300

    remocao dos comentarios

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,3 @@
-/*
-npm -y init
-npm install express dotenv cors
-npm install typescript @prisma/client @types/node @types/express @types/dotenv @types/cors ts-node nodemon -D
-npx prisma init
-npx prisma generate
-npx tsc --init
-npm install yup
-npm install -D @types/yup
-npm run dev
-
--------------------------
-npx prisma studio
-npx prisma migrate dev
-*/
-
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
